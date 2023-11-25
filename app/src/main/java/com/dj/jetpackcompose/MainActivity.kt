bde94586439d77e4ed5dbde3a42ea158ae8cf6a4
Dhanraj-Naik <naik.dhanraj01@gmail.com>
2023-11-25 17:51:03 +0530

    Lesson 1: Composable functions

--- app/src/main/java/com/dj/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/dj/jetpackcompose/MainActivity.kt
@@ -16,32 +16,20 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetPackComposeTheme {
-
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            MessageCard(name = "Carol")
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    JetPackComposeTheme {
-        Greeting("Android")
+    @Composable
+    fun MessageCard(name: String){
+        Text(text = "Patrao $name")
     }
+    
+    @Preview
+    @Composable
+    fun PreviewMessageCard(){
+        MessageCard(name = "Carol")
+    }
+
 }
