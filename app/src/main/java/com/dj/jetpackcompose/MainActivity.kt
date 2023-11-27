d6f770090a04c1c55189116d2f8ea5127f6d5a62
Dhanraj-Naik <naik.dhanraj01@gmail.com>
2023-11-27 13:43:00 +0530

    list and animation

--- app/src/main/java/com/dj/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/dj/jetpackcompose/MainActivity.kt
@@ -4,8 +4,11 @@ import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -14,17 +17,24 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.dj.jetpackcompose.data.Message
+import com.dj.jetpackcompose.sampleData.SampleData
 import com.dj.jetpackcompose.ui.theme.JetPackComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -34,7 +44,8 @@ class MainActivity : ComponentActivity() {
 
             JetPackComposeTheme {
                 Surface(modifier = Modifier.fillMaxSize()) {
-                    MessageCard(Message("Dhanraj", "The learning Art"))
+//                    MessageCard(Message("Dhanraj", "The learning Art"))
+                    Conversation(messages = SampleData.conversationSample)
                 }
             }
         }
@@ -64,21 +75,40 @@ class MainActivity : ComponentActivity() {
             //add horizontal space between image and the column
             Spacer(modifier = Modifier.width(8.dp))
 
+            //// We keep track if the message is expanded or not in this
+            //   variable
+            var isExpanded by remember { mutableStateOf(false) }
+
+            //// surfaceColor will be updated gradually from one color to the other
+            val surfaceColor by animateColorAsState(
+                if(isExpanded) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.surface,
+                label = "Hide Show Preview"
+            )
+
+            // We toggle the isExpanded variable when we click on this Column
             Column {
                 Text(text = "Patrao ${msg.author}",
                     color = MaterialTheme.colorScheme.secondary,
-                    style = MaterialTheme.typography.titleSmall
+                    style = MaterialTheme.typography.titleSmall,
+                    modifier = Modifier.clickable { isExpanded = !isExpanded }
                 )
                 //add vertical space between author and message text
                 Spacer(modifier = Modifier.height(4.dp))
 
                 Surface(
                     shape = MaterialTheme.shapes.extraLarge,
-                    shadowElevation = 1.dp
+                    shadowElevation = 1.dp,
+                    // surfaceColor color will be changing gradually from primary to surface
+                    color = surfaceColor,
+                    // animateContentSize will change the Surface size gradually
+                    modifier = Modifier.animateContentSize().padding(1.dp)
                 ) {
 
                     Text(text = "${msg.body}",
                         modifier = Modifier.padding(all = 4.dp),
+                        // If the message is expanded, we display all its content
+                        // otherwise we only display the first line
+                        maxLines = if (isExpanded) Int.MAX_VALUE else 1 ,
                         style = MaterialTheme.typography.bodyMedium
                     )
                 }
@@ -104,6 +134,24 @@ class MainActivity : ComponentActivity() {
             ) {
                 MessageCard(msg = Message("Dhanraj", "Look at me"))
             }
+        }
+    }
+
+
+    @Composable
+    fun Conversation(messages: List<Message>){
+        LazyColumn{
+            items(messages) { message ->
+                MessageCard(msg = message)
+            }
+        }
+    }
+
+    @Preview
+    @Composable
+    fun PreviewConversation(){
+        JetPackComposeTheme {
+            Conversation(messages = SampleData.conversationSample)
         }
     }
 
